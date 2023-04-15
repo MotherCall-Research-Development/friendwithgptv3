ad2afe80efee726cb28747b598c21e9d3305bb41
0xduc <duncan@ahaslides.com>
2023-04-15 08:27:38 +0700

    add sqs service

--- src/handlers/put-item.js
+++ src/handlers/put-item.js
@@ -1,8 +1,8 @@
 // Create clients and set shared const values outside of the handler
 
 // Create a DocumentClient that represents the query to add an item
-const dynamodb = require('aws-sdk/clients/dynamodb');
-const {v4 : uuid} = require('uuid');
+const dynamodb = require("aws-sdk/clients/dynamodb");
+const { v4: uuid } = require("uuid");
 const docClient = new dynamodb.DocumentClient();
 
 // Get the DynamoDB table name from environment variables
@@ -12,31 +12,29 @@ const tableName = process.env.SAMPLE_TABLE;
  * A simple example includes a HTTP post method to add one item to a DynamoDB table.
  */
 exports.putItemHandler = async (event) => {
-    const { body, httpMethod, path } = event;
-    if (httpMethod !== 'POST') {
-        throw new Error(`postMethod only accepts POST method, you tried: ${httpMethod} method.`);
-    }
-    // All log statements are written to CloudWatch by default. For more information, see
-    // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
-    console.log('received:', JSON.stringify(event));
-    // Get id and name from the body of the request
-    const { question, answer , user  } = JSON.parse(body);
+  const { body, httpMethod, path } = event;
+  if (httpMethod !== "POST") {
+    throw new Error(
+      `postMethod only accepts POST method, you tried: ${httpMethod} method.`
+    );
+  }
 
+  console.log("received:", JSON.stringify(event));
+  // Get id and name from the body of the request
+  const { question, answer, user } = JSON.parse(body);
 
-    const params = {
-        TableName: tableName,
-        Item: { question, answer , id  : uuid(), user },
-    };
-    await docClient.put(params).promise();
-    // add user to the database
+  const params = {
+    TableName: tableName,
+    Item: { question, answer, id: uuid(), user },
+  };
+  await docClient.put(params).promise();
+  // add user to the database
 
-   
+  const response = {
+    statusCode: 200,
+    body,
+  };
 
-    const response = {
-        statusCode: 200,
-        body,
-    };
-
-    return response;
+  return response;
 };
-// 
+//
