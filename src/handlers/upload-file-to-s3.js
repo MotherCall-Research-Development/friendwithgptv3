d1b8d13643378da4a216b5290837f94cb29e7ee5
0xduc <duncan@ahaslides.com>
2023-04-15 14:40:51 +0700

    task: improve s3 upload

--- src/handlers/upload-file-to-s3.js
+++ src/handlers/upload-file-to-s3.js
@@ -2,16 +2,16 @@
 
 // Create a client to read objects from S3
 const AWS = require("aws-sdk");
-
+const { v4: uuid } = require("uuid");
 const s3 = new AWS.S3();
-
+AWS.config.update({ region: "ap-southeast-1" });
 exports.uploadFileToS3Handler = async (event, context) => {
   const bucketName =
-    "aws-ap-southeast-1-162229977653-friendwithgptv3-simpleappbucket";
+    "aws-ap-southeast-1-162229977653-friendwithgptv3-friendGptBucket";
   // upload file to s3 , in body  method post
-  console.log("bucketName", bucketName);
-  const fileData = Buffer.from(event.body, "base64");
-  const fileName = event.pathParameters.filename;
+  const fileData = Buffer.from(event.body.file, "base64");
+  const fileName = uuid();
+
   const s3Params = {
     Bucket: bucketName,
     Key: fileName,
